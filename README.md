6420ac5e0f885e8ed8443cba46132d562d0b1998
Taejin Kim <tj4467@naver.com>
2022-10-12 11:04:53 +0900

    stop

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-패스오더 지원서
-
 redux toolkit 사용
--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -11,14 +11,17 @@ const Intro = () => {
     <div className={'intro-box'+(fold?'':' intro-padding')}>
       <div>
         <img src={photo} className="intro-photo"/>
-        </div>
-        <div>
-          개발자가 된 이유<br/>
-          프론트엔드는 직관적으로 눈으로 볼 수 있으며<br/>
-          개발자라는 직업은 새로운 것을 접하는것을<br/>
-          좋아하는 저에게 즐거움을 선사해주기에 선택하였습니다.<br/>
-        </div>
-        
+      </div>
+      <div className='introduction'>
+          이름 : 김태진<br/>
+          나이 : 30세<br/>
+          전공 : 전기공학과<br/>
+
+      </div>
+      <div>
+        <h2>개발자가 된 이유</h2>
+        프론트엔드는 직관적으로 눈으로 볼 수 있으며 개발자라는 직업은 새로운 것을 접하는것을 좋아하는 저에게 즐거움을 선사해주기에 선택하였습니다.
+      </div>
 
     </div>
   )
